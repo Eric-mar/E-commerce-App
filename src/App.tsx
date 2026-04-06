34a17cb9f8053a9090f1280c9e051ffbcd0061dd
tuyishime eric <erictuyishime2004@gmail.com>
2026-04-06 16:39:55 +0200

    adding base files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(()=>{
+    const fetching = async() =>{
+      try {
+        const res = await fetch(`https://api.escuelajs.co/api/v1/categories`)
+        if(!res.ok){
+          throw new Error('failed to fetch!!')
+        }
+        const data = await res.json()
+        console.log(data)
+      } catch (error) { console.error(error)
+        
+      }
+    }
+     fetching()
+  },[])
+
+
   return (
-    <div>App</div>
+    <>
+    <div>
+      {}
+
+    </div>
+    </>
   )
 }
 
 export default App
+
+
